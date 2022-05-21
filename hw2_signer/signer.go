66609bf4fd6e696d26ad850242542a1a2633a531
kalunik <nikitaex123@gmail.com>
2022-05-21 21:09:44 +0300

    TestPipeline failed

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -10,30 +10,51 @@ func ExecutePipeline(FlowJobs ...job) {
 	var in, out chan interface{}
 	var wg sync.WaitGroup
 
-	for k, j := range FlowJobs {
+	for _, j := range FlowJobs {
 		in = out
-		out = make(chan interface{})
-		fmt.Println(k, "||", j)
+		out = make(chan interface{}, 1)
+		//var out chan interface{}
+		//fmt.Println(cap(out), "fds")
+		//fmt.Println(k, "||", j)
 		wg.Add(1)
 		go func(j job, in, out chan interface{}) {
-			j(in, out)
-			defer wg.Done() //without
+			defer wg.Done()
+			//defer close(in)
 			defer close(out)
+			j(in, out)
 		}(j, in, out)
 		wg.Wait()
 	}
 }
 
 func main() {
-	FlowJobs := []job{
+	//FlowJobs := []job{
+	//	job(func(in, out chan interface{}) {
+	//		//out <- 2
+	//		fmt.Println("in: " /*<-in,*/, " | 1 func executed")
+	//	}),
+	//	job(func(in, out chan interface{}) {
+	//		//out <- 3
+	//		fmt.Println("2 func executed. ", "in: " /*, <-in*/)
+	//	}),
+	//}
+	//ExecutePipeline(FlowJobs...)
+
+	FreeFlowJobs := []job{
 		job(func(in, out chan interface{}) {
-			//out <- 2
-			fmt.Println("in: " /*<-in,*/, " | 1 func executed")
+			fmt.Println("first ", "cap", cap(out), "ptr", out)
+			out <- "Hello"
 		}),
 		job(func(in, out chan interface{}) {
-			//out <- 3
-			fmt.Println("2 func executed. ", "in: " /*, <-in*/)
+			fmt.Println("second", "cap", cap(out), "ptr", out)
+			input := fmt.Sprintf("%v", <-in)
+			input += " world"
+			out <- input
+		}),
+		job(func(in, out chan interface{}) {
+			fmt.Println("third ", "cap", cap(out), "ptr", out)
+			fmt.Println("Full string:", <-in)
 		}),
 	}
-	ExecutePipeline(FlowJobs...)
+	ExecutePipeline(FreeFlowJobs...)
 }
