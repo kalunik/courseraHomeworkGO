7b41152647de3d3e70fa601cc9602dc008e62aea
kalunik <nikitaex123@gmail.com>
2022-05-26 22:52:30 +0300

    CombineResult put in out not that expected

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 	"sync"
 )
 
@@ -25,23 +27,45 @@ func ExecutePipeline(FlowJobs ...job) {
 }
 
 var SingleHash = func(in, out chan interface{}) {
-	var (
-		crcMd,
-		crc string
-	)
 	for v := range in {
 		data := fmt.Sprint(v)
 		Md := DataSignerMd5(data)
 		crcMd := DataSignerCrc32(Md)
 		crc := DataSignerCrc32(data)
-		fmt.Println(data, " SingleHash data ", data)
-		fmt.Println(data, " SingleHash md5(data) ", Md)
-		fmt.Println(data, " SingleHash crc32(md5(data)) ", crcMd)
-		fmt.Println(data, " SingleHash crc32(data) ", crc)
-		fmt.Printf(" SingleHash result ", crc, "~", crcMd)
+		fmt.Println(data, "SingleHash data", data)
+		fmt.Println(data, "SingleHash md5(data)", Md)
+		fmt.Println(data, "SingleHash crc32(md5(data))", crcMd)
+		fmt.Println(data, "SingleHash crc32(data)", crc)
+		fmt.Printf("%s SingleHash result %s~%s\n", data, crc, crcMd)
+		out <- fmt.Sprintf("%s~%s", crc, crcMd) // out must be in range, so then PANIC happen
 	}
-	out <- fmt.Sprintf("%s~%s", crc, crcMd) // out must be in range, so then PANIC happen
+}
 
+var MultiHash = func(in, out chan interface{}) {
+	var (
+		tmp, crc string
+		res      []string
+	)
+	for v := range in {
+		data := fmt.Sprint(v)
+		for th := 0; th < 6; th++ {
+			crc = DataSignerCrc32(fmt.Sprintf("%d%s", th, data))
+			fmt.Println(data, "MultiHash: crc32(th+step1)) ", th, crc)
+			tmp += crc
+		}
+		res = append(res, tmp)
+		fmt.Println(data, "MultiHash result:", tmp)
+		tmp = ""
+	}
+	out <- res
+}
+
+var CombineResults = func(in, out chan interface{}) {
+	v := <-in
+	data := v.([]string)
+
+	sort.Strings(data)
+	out <- strings.Join(data, "_")
 }
 
 func main() {
@@ -75,10 +99,10 @@ func main() {
 		}
 		ExecutePipeline(FreeFlowJobs...)*/
 
-	testExpected := "1173136728138862632818075107442090076184424490584241521304_1696913515191343735512658979631549563179965036907783101867_27225454331033649287118297354036464389062965355426795162684_29568666068035183841425683795340791879727309630931025356555_3994492081516972096677631278379039212655368881548151736_4958044192186797981418233587017209679042592862002427381542_4958044192186797981418233587017209679042592862002427381542"
+	testExpected := "27225454331033649287118297354036464389062965355426795162684_29568666068035183841425683795340791879727309630931025356555"
 	testResult := "NOT_SET"
 
-	inputData := []int{0, 1}
+	inputData := []int{0, 2}
 
 	hashSignJobs := []job{
 		job(func(in, out chan interface{}) {
@@ -87,10 +111,11 @@ func main() {
 			}
 		}),
 		job(SingleHash),
-		/*job(MultiHash),
-		job(CombineResults),*/
+		job(MultiHash),
+		job(CombineResults),
 		job(func(in, out chan interface{}) {
 			dataRaw := <-in
+			fmt.Println("!!", dataRaw)
 			data, ok := dataRaw.(string)
 			if !ok {
 				_ = fmt.Errorf("cant convert result data to string")
@@ -102,6 +127,6 @@ func main() {
 	ExecutePipeline(hashSignJobs...)
 
 	if testExpected != testResult {
-		_ = fmt.Errorf("results not match\nGot: %v\nExpected: %v", testResult, testExpected)
+		fmt.Printf("results not match\nGot: %v\nExpected: %v", testResult, testExpected)
 	}
 }
